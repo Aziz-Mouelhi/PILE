d2e83bc7797c5a51d106974a6657d3581be9064e
unknown <aziz11mouelhi@gmail.com>
2024-03-16 14:36:43 +0000

    README

--- Pile/Pile.h
+++ Pile/Pile.h
@@ -1,21 +1,60 @@
 #ifndef PILE_H_INCLUDED
 #define PILE_H_INCLUDED
-#define LongMax 100 /* longueur maximale d'une pile */
+#define Max 100 /* longueur maximale d'une pile */
 
-typedef struct {
-int sommet;
-int taille;
-int pile[LongMax];
-}mypile*Mypile;
+typedef struct pile{
+int top;
+int arr_stack[Max];
+int Size;
 
-int estvide(mypile p)
-{
-    return p.sommet==0;
+}pile;
+
+
+/////creare functoin////
+pile *CreatePile(){
+        pile *pointer;
+        pointer = malloc(sizeof(pile));
+        pointer->top = -1 ;
+        pointer->Size = 0 ;
+        return pointer;
 }
-void empiloler(mypile *p , int value)
-{
-    p->sommet++;
-    p->pile[p->sommet]= value;
+///////push function////:
+void Push(pile *Pointer,int value){
+        if(Pointer->Size == Max)
+            {
+                printf("insufficient space");
+            }
+        else
+        {
+            Pointer->top++;
+            Pointer->arr_stack[Pointer->top]=value;
+            Pointer->Size++;
+        }
 }
-
+/////pop function////////////
+int pop(pile *Pointer){
+    if(Pointer->Size == 0){
+        printf("The Stack Already Epmty");
+    }else{
+        int val ;
+        val = Pointer->arr_stack[Pointer->top];
+        Pointer->arr_stack[Pointer->top]=0;
+        Pointer->top--;
+        Pointer->Size--;
+        return val;
+    }
+}
+void Print(pile *Pointer){
+    if(IsFull(*Pointer)){
+        printf("Pile is empty :\n");
+    }else{
+        int i;
+        for(i=Pointer->top;i>=0 ;i--){
+            printf("Element :%d\n",Pointer->arr_stack[i]);
+        }
+    }
+}
+    int IsFull(pile *pointer){
+        return pointer->Size==Max;
+    }
 #endif // PILE_H_INCLUDED
--- Pile/main.c
+++ Pile/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include"pile.h"
+
+
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    pile *mypile;
+    int popval;
+
+mypile = CreatePile();
+Push(mypile,3);
+Push(mypile,23);
+Push(mypile,31);
+Push(mypile,10);
+
+printf("the first element in stack :%d\n",mypile->arr_stack[mypile->top]);
+printf(" the size of stack :%d\n",mypile->Size);
+    popval= pop(mypile);
+    printf("element %d has removed :\n",popval);
+    printf(" the size of stack :%d\n",mypile->Size);
+    printf("print pile\n");
+    Print(mypile);
 }
